f664c569e84693783d148d7bbbb00af8cef8d5da
yeonkyeong-lee <43263913+yeonkyeong-lee@users.noreply.github.com>
2021-03-23 13:31:39 +0900

    Update main.cpp

--- homework/01.Transformations_glm/cpp/skeleton/main.cpp
+++ homework/01.Transformations_glm/cpp/skeleton/main.cpp
@@ -137,15 +137,21 @@ std::ostream &operator<< (std::ostream &os, const glm::vec3 v) {
   os << "[";
   os << v.x << ", " << v.y << ", " << v.z;
   os << "]";
+  
+  return os;
 }
 std::ostream &operator<< (std::ostream &os, const glm::vec4 v) {
   os << "[";
   os << v.x << ", " << v.y << ", " << v.z << ", " << v.w;
   os << "]";
+  
+  return os;
 }
 std::ostream &operator<< (std::ostream &os, const glm::mat4 m) {
   os << "[" << m[0][0] << ", " << m[1][0] << ", " << m[2][0] << ", " << m[3][0] << "]" << std::endl;
   os << "[" << m[0][1] << ", " << m[1][1] << ", " << m[2][1] << ", " << m[3][1] << "]" << std::endl;
   os << "[" << m[0][2] << ", " << m[1][2] << ", " << m[2][2] << ", " << m[3][2] << "]" << std::endl;
   os << "[" << m[0][3] << ", " << m[1][3] << ", " << m[2][3] << ", " << m[3][3] << "]" << std::endl;
+  
+  return os;
 }
